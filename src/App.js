9de61dd9b355cf7179c318cfd834165625eee8f1
Moss <mossc2001@gmail.com>
2023-02-03 15:31:55 -0800

    finished read, create, update, and delete functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {React, useState} from 'react'
+import {React, useState, useEffect} from 'react'
 import { Routes, Route } from "react-router-dom"
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -8,18 +8,60 @@ import ReptileShow from './pages/ReptileShow'
 import ReptileNew from './pages/ReptileNew'
 import ReptileEdit from './pages/ReptileEdit'
 import NotFound from './pages/NotFound'
-import mockReptiles from './mockReptiles'
 import './App.css'
 
 const App = () => {
-  const [reptiles, setReptiles] = useState(mockReptiles)
+  const [reptiles, setReptiles] = useState([])
+
+  useEffect(() => {
+    readReptile()
+  }, [])
+
+  const readReptile = () => {
+    fetch("http://localhost:3000/reptiles")
+    .then((response) => response.json())
+    .then((payload) => {
+      setReptiles(payload)
+    })
+    .catch((error) => console.log(error))
+  }
 
   const createReptile = (reptile) => {
-    console.log(reptile);
+    fetch("http://localhost:3000/reptiles", {
+      body: JSON.stringify(reptile),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST"
+    })
+    .then((response) => response.json())
+    .then((payload) => readReptile())
+    .catch((errors) => console.log("Reptile create errors:", errors))
   }
   
   const updateReptile = (reptile, id) => {
-    console.log("reptile:", reptile);
+    fetch(`http://localhost:3000/reptiles/${id}`, {
+      body: JSON.stringify(reptile),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "PATCH"
+    })
+    .then((response) => response.json())
+    .then((payload) => readReptile())
+    .catch((errors) => console.log("Reptile update errors:", errors))
+  }
+
+  const deleteReptile = (id) => {
+    fetch(`http://localhost:3000/reptiles/${id}`, {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "DELETE"
+    })
+    .then((response) => response.json())
+    .then((payload) => readReptile())
+    .catch((errors) => console.log("Reptile delete errors:", errors))
   }
 
   return(
@@ -29,7 +71,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/reptileindex" element={<ReptileIndex reptiles={reptiles}/>} />
-        <Route path="/reptileshow/:id" element={<ReptileShow reptiles={reptiles}/>} />
+        <Route 
+          path="/reptileshow/:id" 
+          element={<ReptileShow reptiles={reptiles} deleteReptile={deleteReptile}/>} 
+        />
         <Route path="/reptilenew" element={<ReptileNew createReptile={createReptile} />} />
         <Route path="/reptileedit/:id" element={<ReptileEdit reptiles={reptiles} updateReptile={updateReptile} />} />
         <Route path="*" element={<NotFound />} />
