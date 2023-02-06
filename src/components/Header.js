cfdb2d2fee6e96b1bfe34240d14620118d3cb54d
James Quillen <james.quillen@gmail.com>
2023-02-06 08:59:55 -0800

    Added header and homepage styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,51 @@
-const Header = () => {
-    return(
-      <>
-        <div className="header">
-            Welcome to Reptile Tinder
-        </div>
-      </>
-    )
-  }
-  export default Header;
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
+import Logo from '../assets/hotel.jpg'
+
+function Header() {
+
+  return (
+    <>
+      <Navbar
+        className='border-bottom bg-success'
+      >
+        <NavbarBrand href="/">
+          <img
+            alt="gecko"
+            src={Logo}
+            style={{
+              height: 40,
+              width: 80
+            }}
+          />
+          <NavbarText className='nav-text'>
+            Reptile Tinder
+          </NavbarText>
+      </NavbarBrand>
+            <Nav className='ml-auto'>
+              <NavItem className='nav-link-text'>
+                <NavLink href="/reptileindex/" className='link-dark'>See Our Reptiles</NavLink>
+              </NavItem>
+              <NavItem className='nav-link-text'>
+                <NavLink href="/reptilenew/" className='link-dark'>Create a Profile</NavLink>
+              </NavItem>
+            </Nav>
+      </Navbar>
+    </>
+  );
+}
+
+export default Header;
